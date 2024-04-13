c5ca3cb4de1c3632a10aa88b1e2cebfeb70942fc
kvnmoorthy <murthy298@gmail.com>
2024-04-14 05:00:26 +0530

    Update providers.tf

--- hello project/providers.tf
+++ hello project/providers.tf
@@ -1,6 +1,8 @@
 provider "azurerm" {
   features {}
-
+  tenant_id       = "687bbf51-433c-425f-a802-91290c993f57"
+  subscription_id = "3c850cea-049e-4bff-a039-406dc6fafa3c"
+  client_id       = "478a6218-74a3-4798-ae56-34a99d9e1174"
 
 }
 
